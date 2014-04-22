6b6ffd8360f1d90cc40b0bcd78ad09a96c13bcc9
waternik <Aqua@nxt.ru>
2014-04-22 13:51:01 +0400

    today

---
+++ FirstWebApp/FirstWebApp/Models/AccountModels.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace FirstWebApp.Models
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<RegistratedMember> RegistratedMembers { get; set; }
+    }
+
+    [Table("UserProfile")]
+    public class UserProfile
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+
+    [Table("RegistratedMembers")]
+    public class RegistratedMember
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("UserProfile")]
+        public int UserId { get; set; }
+        public virtual UserProfile UserProfile { get; set; }
+
+        public DateTime DateRegistration { get; set; }
+    }
+
+    public class RegisterExternalLoginModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        public string ExternalLoginData { get; set; }
+    }
+
+    public class LocalPasswordModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Текущий пароль")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Пароль должен содержать не менее {2} символов.", MinimumLength = 3)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Новый пароль")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Повторите новый пароль")]
+        [Compare("NewPassword", ErrorMessage = "Пароли не сопадают, повторите пожалуйста ввод.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить меня?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Пароль должен содержать не менее {2} символов.", MinimumLength = 3)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Новый пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Повторите новый пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не сопадают, повторите пожалуйста ввод.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ExternalLogin
+    {
+        public string Provider { get; set; }
+        public string ProviderDisplayName { get; set; }
+        public string ProviderUserId { get; set; }
+    }
+}
