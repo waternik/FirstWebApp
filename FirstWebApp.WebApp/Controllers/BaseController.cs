fec53660d3649645cf7a92a6b6ef7f18cfb22cdb
waternik <Aqua@nxt.ru>
2014-05-06 11:47:42 +0400

    Added log4net to Controlers

--- FirstWebApp.WebApp/Controllers/BaseController.cs
+++ FirstWebApp.WebApp/Controllers/BaseController.cs
@@ -1,14 +1,20 @@
 ﻿using FirstWebApp.WebApp.Filters;
 using System.Web.Mvc;
+using log4net;
+using log4net.Config;
 
 namespace FirstWebApp.WebApp.Controllers
 {
+    using log4net.Util;
+
     [Culture]
     public class BaseController : Controller
     {
-        [Culture]
+        public static readonly ILog log = LogManager.GetLogger(typeof(MvcApplication));
+
         protected override void OnException(ExceptionContext exceptionContext)
         {
+            log.ErrorExt(exceptionContext.Exception.Message);
             var e = exceptionContext.Exception;
             exceptionContext.ExceptionHandled = true;
             exceptionContext.Result = new ViewResult { ViewName = MVC.Shared.Views.ErrorView };
--- FirstWebApp.WebApp/Global.asax.cs
+++ FirstWebApp.WebApp/Global.asax.cs
@@ -3,28 +3,26 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
+using log4net.Config;
 
 namespace FirstWebApp.WebApp
 {
-    using FirstWebApp.WebApp.Logger;
-
-    using log4net;
 
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
     public class MvcApplication : HttpApplication
     {
-        private static readonly ILog Logger = LoggerFactory.GetLogger("Global"); 
+        public static readonly ILog log = LogManager.GetLogger(typeof(MvcApplication));
 
         protected void Application_Start()
         {
-            Logger.Debug("Application_Start");
+            XmlConfigurator.Configure();
 
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
-            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
